32b9b3b79cef92d42b7c5a3ebaa68ead981abe6e
safvanasafu292-afk <safvanasafu292@gmail.com>
2026-08-08 21:54:05 +0530

    calaculator project

---
+++ calulator_error_handling.py
@@ -0,0 +1,32 @@
+def calculator():
+    try:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+        op = input("Enter operator (+, -, *, /): ")
+
+        if op == '+':
+            print("Result:", a + b)
+
+        elif op == '-':
+            print("Result:", a - b)
+
+        elif op == '*':
+            print("Result:", a * b)
+
+        elif op == '/':
+            print("Result:", a / b)
+
+        else:
+            raise ValueError("Invalid operator!")
+
+    except ValueError as e:
+        print("Error:", e)
+
+    except ZeroDivisionError:
+        print("Error: Division by zero not allowed!")
+
+    finally:
+        print("Thank you for using the calculator.")
+
+
+calculator()
